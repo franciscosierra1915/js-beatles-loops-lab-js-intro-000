a6e140f75e948698b364fb33f4454d2e9f7bda5f
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-26 18:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var empty = [];
+  for (var i = 0; i < arrayMusicians.length; i++) {
+    empty.push(arrayMusicians[i] +" " + "plays" + " " + arrayInstruments[i]);
+  }
+  return empty;
+}
+function johnLennonFacts(facts) {
+  var empty = [];
+  var i = 0;
+  while (empty.length < facts.length) {
+    empty.push(facts[i] + "!!!");
+    i++;
+  }
+  return empty;
+}
